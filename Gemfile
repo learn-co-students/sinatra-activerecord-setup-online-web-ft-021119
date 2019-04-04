2ef4f6fe36c079fb13db0ab8e56bbb611e093a5f
Taylor Swinton <taylorswinton1@gmail.com>
2019-04-04 16:08:47 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -12,10 +12,15 @@ gem 'require_all'
 group :development do
 	gem 'shotgun'
 	gem 'pry'
+<<<<<<< HEAD
   gem 'tux'
   gem 'sqlite3', '~> 1.3.7'
+=======
+	gem 'tux'
+	gem 'sqlite3'
+>>>>>>> 4e482673706b59e593b7b476510b89d33b9b1094
 end
-    
+
 group :test do
   gem 'rspec'
   gem 'capybara'
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+<<<<<<< HEAD
 configure :development do 
     set :database, 'sqlite3:db/database.db'
 end
@@ -10,3 +11,10 @@ end
 
 require './app'
 
+=======
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+
+require './app'
+>>>>>>> 4e482673706b59e593b7b476510b89d33b9b1094
